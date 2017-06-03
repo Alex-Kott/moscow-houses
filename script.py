c39e5f41dccb8198a916b89ae343adfac7359ea5
AlexKott <alexey.kott@gmail.com>
2017-06-03 12:04:43 +0300

    fdvjofidjv

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import re
 import requests as req
 from bs4 import BeautifulSoup
 import xlsxwriter
+import time
 
 
 with open("street_dict.pickle", "rb") as f:
@@ -12,19 +13,23 @@ f.close()
 keys = street_dict.keys()
 
 for i in keys:
-	url = "http://tipdoma.ru/search_bld.php?id="+str(i)+"&value="+street_dict[i]
-	r = req.get(url)
+	url = "http://tipdoma.ru/search_bld.php"
+	print(url)
+	#print(str(i)+"   "+street_dict[i])
+	r = req.get(url, {'id' : i, 'value' : street_dict[i]})
+	print(r.url)
 	r.encoding = "UTF-8"
 	S = r.text
-	if S.find("Не найдено") == -1:
-		print(str(i)+"  "+street_dict[i])
-	table = S[S.find("<table"):]
-	#print(table)
+	#print(S)
+	if S.find("Не найдено") != -1:
+		continue
 	soup = BeautifulSoup(r.text, "lxml")
-	trs = soup.find_all("tr")
-	#for i in trs:
-		#tds = trs.findall("td")
-		#print(i)
+	print(r.text)
+	'''table = soup.table
+	trs = table.tr
+	print(table)'''
+	time.sleep(1)
+
 
 '''
 for i in street_dict:
